5fffb81248c9e8d417cb3e69a4553b13ece7c108
Abimbola <Haryishar2@gmail.com>
2025-02-12 03:05:49 +0100

    task

---
+++ classWork/Task Five/Company.test.js
@@ -0,0 +1,14 @@
+const company = {
+    name: "TechCorp",
+    location: "San Francisco",
+    employees: [
+        {id: 1, name: "ayo", department: "Engineering"},
+        {id: 2, name: "bolaji", department: "HR"},
+        {id: 3, name: "tayo", department: "Marketing"},
+        {id: 4, name: "abefe", department: "HR"},
+    ]
+};
+
+test ("print the name of the second employee", () => {
+    let
+})
